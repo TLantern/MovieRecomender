e191cc0d2c5b4a1b97cf2c5fa77043e54f75e3b7
Ten <pgtherealmvp@gmail.com>
2025-07-22 16:39:08 -0600

    Fix 2

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ app = FastAPI(
 # ==== CORS Middleware ====
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cypress‑hide‑0280.typedream.app"],    
-    allow_credentials=True,  
+    allow_origins=["*"],         
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
